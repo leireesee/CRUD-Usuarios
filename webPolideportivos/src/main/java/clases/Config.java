b45e9f3571819d3829f9458b188c9269457135f6
plaiaundi <plaiaundi@HZ114833.H012108.NET>
2023-03-22 13:50:22 +0100

    s

--- webPolideportivos/src/main/java/clases/Config.java
+++ webPolideportivos/src/main/java/clases/Config.java
@@ -1,7 +1,7 @@
 package clases;
 
 public interface Config {
-	public static final String BBDD = "polideportivo";
+	public static final String BBDD = "polideportivos";
     public static final String HOST = "localhost";
     public static final String USERNAME = "root";
     public static final String PASSWORD = "";
